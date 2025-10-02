ba7483494818a95bf4712be49e7020497efa0b29
piitrii <piitrii@gmail.com>
2025-10-02 18:28:44 +0300

    Added a loop for Main.py

--- Main.py
+++ Main.py
@@ -20,40 +20,46 @@ from PickWordsFromText import pick_words_interactive
 
 # Jos ohjelmaa ajaa terminaalin kautta, niin tulee ensin mennä WordWhiz-kansioon, jotta ohjelma löytää ennakkoon muodostetut sanastot Chapter_1.csv jne. 
 def main():
-    print("Tervetuloa sanapeliin!")
-    print("Valitse vaihtoehto:")
-    print("1–5: Käytä valmista sanastoa syöttämällä haluamasi kappaleen numero")
-    print("t: Käytä valmista sanastoa syöttämällä haluamasi tiedoston nimi (esim. sanakoe.csv)")
-    print("p: Liitä oma teksti, josta sanat poimitaan ja tallennetaan csv-muodossa")
+    while True:
+        print("Tervetuloa sanapeliin!")
+        print("Valitse vaihtoehto:")
+        print("1–5: Käytä valmista sanastoa syöttämällä haluamasi kappaleen numero")
+        print("t: Käytä valmista sanastoa syöttämällä haluamasi tiedoston nimi (esim. sanakoe.csv)")
+        print("p: Liitä oma teksti, josta sanat poimitaan ja tallennetaan csv-muodossa")
 
-    valinta = input("Valintasi: ").strip()
+        valinta = input("Valintasi: ").strip()
 
-    if valinta.lower() == "t":
-        tiedostonimi = input("Anna CSV-tiedoston nimi: ").strip()
-        try:
-            sanasto = lataa_sanasto(tiedostonimi, tiedostona=True)
-        except FileNotFoundError:
-            print(f"Tiedostoa '{tiedostonimi}' ei löytynyt.")
-            return
-    
-    elif valinta.lower() == "p":
-            pick_words_interactive()
-            return
-    
-    else:
-        try:
-            num = int(valinta)
-            if not 1 <= num <= 5:
-                print("Valitse numero 1–5.")
+        if valinta.lower() == "t":
+            tiedostonimi = input("Anna CSV-tiedoston nimi: ").strip()
+            try:
+                sanasto = lataa_sanasto(tiedostonimi, tiedostona=True)
+            except FileNotFoundError:
+                print(f"Tiedostoa '{tiedostonimi}' ei löytynyt.")
                 return
-            sanasto = lataa_sanasto(num)
-        except ValueError:
-            print("Virheellinen valinta.")
-            return
+        
+        elif valinta.lower() == "p":
+                pick_words_interactive()
+                return
+        
+        else:
+            try:
+                num = int(valinta)
+                if not 1 <= num <= 5:
+                    print("Valitse numero 1–5.")
+                    return
+                sanasto = lataa_sanasto(num)
+            except ValueError:
+                print("Virheellinen valinta.")
+                return
 
-    kysy_sanoja(sanasto)
+        kysy_sanoja(sanasto)
+
+        print("\nHaluatko pelata uudelleen? (kyllä/ei)")
+        response = input().strip().lower()
+
+        if response != 'kyllä':
+            print("Kiitos pelaamisesta!")
+            break
 
 if __name__ == "__main__":
     main()
-
-input("\nPress Enter to exit...")
